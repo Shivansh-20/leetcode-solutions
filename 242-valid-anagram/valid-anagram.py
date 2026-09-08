dabeffe04dbca32499dad3a64750a025b0b69abf
Shivansh Tyagi <135636253+Shivansh-20@users.noreply.github.com>
2026-09-08 07:18:50 +0530

    Time: 901 ms (6.01%) | Memory: 19.2 MB (99.35%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,14 +1,11 @@
-'''class Solution:
+class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        for c in set(s):
-                if s.count(c) != t.count(c):
-                    return False
+        for i in s:
+            if s.count(i) != t.count(i):
+                return False
+        
+        return True     
 
-        return True'''
-
-from collections import Counter
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        
