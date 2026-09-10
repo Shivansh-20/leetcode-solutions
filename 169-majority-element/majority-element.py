400e67ac5582066dffd102112cec0a4eafa3b284
Shivansh Tyagi <135636253+Shivansh-20@users.noreply.github.com>
2026-09-10 13:23:13 +0530

    Time: 15 ms (18.95%) | Memory: 21.1 MB (47.37%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -2,10 +2,7 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         seen = {}
         for i in nums:
-            if i in seen:
-                seen[i] += 1
-            else:
-                seen[i] = 1
+            seen[i] = seen.get(i,0) + 1  #Give me the value associated with key i. If i doesn't exist, #give me 0 , add + 1 to it 
             n = len(nums)
             if seen[i] > (n/2):
                 return i
